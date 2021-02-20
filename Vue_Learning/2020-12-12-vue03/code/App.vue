6432a4d5d4be8d925b21e654181724626def6b55
Csy <838236432@qq.com>
2021-02-20 23:48:58 +0800

    First commit

---
+++ Vue_Learning/2020-12-12-vue03/code/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <ul>
+      <li>{{ count }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+// js
+export default {
+  data() {
+    return {
+      count: 1,
+    };
+  },
+};
+</script>
+
+<style>
+//
+</style>
