1c212cf11c1e7c854f6af0e053168de9356c7e47
Csy <838236432@qq.com>
2021-03-11 20:38:45 +0800

    2021-3-11_Leetcode

--- Algorithm/LeetCode/283. 移动零.js
+++ Algorithm/LeetCode/283. 移动零.js
@@ -2,33 +2,17 @@
  * @param {number[]} nums
  * @return {void} Do not return anything, modify nums in-place instead.
  */
-// var moveZeroes = function (nums) {
-//     var i = 0;
-//     while (i < nums.length) {
-//         if(nums[i] == 0){
-//             nums.splice(i,1);
-//             nums.push(0);
-//             console.log(nums)
-//         }
-//         i++;
-//     }
-//     return nums;
-// };
-
-// console.log(moveZeroes( [0,1,0,3,12]));
-
-// console.log(moveZeroes( [0,1,0]));
-
-// console.log(moveZeroes( [0,0,1]));
-nums = [0,0,1];
-var i = 0;
-while (i < nums.length) {
-    if(nums[i] == 0){
-        nums.splice(i,1);
-        nums.push(0);
-        i = 0;
-    }else{
-        i ++;
+ var moveZeroes = function (nums) {
+    var i = 0;
+    var count = 0;
+    while (count < nums.length) {
+        if (nums[i] == 0) {
+            nums.splice(i, 1);
+            nums.push(0);
+        } else {
+            i++;
+        }
+        count++;
     }
-    
-}
+    return nums;
+};
