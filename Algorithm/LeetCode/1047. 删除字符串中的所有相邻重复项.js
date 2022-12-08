24642926f126b8d54eefe88ca88c28d9f1c79ab5
ChaVladimir <59714086+ChanSuyuan@users.noreply.github.com>
2022-12-08 18:52:32 +0800

    #3131221321 fixasdawd

--- Algorithm/LeetCode/1047. 删除字符串中的所有相邻重复项.js
+++ Algorithm/LeetCode/1047. 删除字符串中的所有相邻重复项.js
@@ -29,3 +29,65 @@ var removeDuplicates = function(S) {
 
 console.log(removeDuplicates("abbaca"));
 
+/**
+ * @param {string} S
+ * @return {string}
+ */
+// var removeDuplicates = function (S) {
+//     let len = S.length;
+//     let cache = S.split("");
+//     for (let i = 0; i < len; i++) {
+//         if (cache[i] == cache[i + 1]) {
+//             cache.splice(i, i + 1);
+//         }
+//     }
+//     return cache;
+
+// };
+// console.log(removeDuplicates("abbaca"));
+
+var removeDuplicates = function(S) {
+    const stk = [];
+    for (const ch of S) {
+        if (stk.length && stk[stk.length - 1] === ch) {
+            stk.pop();
+        } else {
+            stk.push(ch);
+        }
+    }
+    return stk.join('');
+};
+
+console.log(removeDuplicates("abbaca"));
+
+/**
+ * @param {string} S
+ * @return {string}
+ */
+// var removeDuplicates = function (S) {
+//     let len = S.length;
+//     let cache = S.split("");
+//     for (let i = 0; i < len; i++) {
+//         if (cache[i] == cache[i + 1]) {
+//             cache.splice(i, i + 1);
+//         }
+//     }
+//     return cache;
+
+// };
+// console.log(removeDuplicates("abbaca"));
+
+var removeDuplicates = function(S) {
+    const stk = [];
+    for (const ch of S) {
+        if (stk.length && stk[stk.length - 1] === ch) {
+            stk.pop();
+        } else {
+            stk.push(ch);
+        }
+    }
+    return stk.join('');
+};
+
+console.log(removeDuplicates("abbaca"));
+
